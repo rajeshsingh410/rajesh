cdbb1f42d57111977b83b8cda292cef60c070acf
rajeshsingh410 <rajesh6266969294@gmail.com>
2023-11-03 10:13:17 +0530

    home added

---
+++ HOME.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en" >
+<link rel="icon" type="text/css" href="RAVI.png">
+<head>
+	<title>_RAVI_ENTERPRISE_</title>
+	
+	<style>
+body {
+  background-image: url('background.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed; 
+  background-size: 100% 100%;
+}
+</style>
+</head>
+<body>
+
+	<div class="container">
+		 		<table>
+		 				<tr >
+                <th style="color: "><img src="RAVI.png" id="img4" class="leftlogo"></th>
+                  <td style="width:100%"> <h1>_welcom_TO_R@VI_ENTERPRISE__</h1>
+                  	 <img src="login.png"alt="Trulli" width="50%" height="10%" style="float: right;height: 50px;width: 50px;margin-top:-41px"> 
+                  	 <p style="float: right;margin-right: -41px">log.in </p>
+                     <h2>_THE_SMART_POINT_</h2>
+                        <h3>_sales_and_service_</h3>
+
+                    </td>
+
+		 			  </tr>
+                    </table>
+		 	          </div>
+ <div class="container">
+ 	<table border="20" style="width:100%" class="table table-hover">
+  <tr style="background-color:red">
+  <th style="color: "> <h1>WEL_COME_TO_MOBILE_JAKSHAN_</h1>  </th>
+  </tr>
+</table>
+ </div>
+ 
+ <div class="container">
+ 	<table border="2" style="width:100%" class="table table-hover">
+  <tr style="background-color: #0000ff">
+  <td>Mobiles </td>
+  <td>Tablets </td>
+  <td>Laptops </td>
+  <td>Computers </td>
+  <td>News paper </td>
+  <td>Accessories </td>
+  </tr>
+ 
+</table>
+ </div>
+<table style="width: 100%" class="table table-hover">
+  <tr style="background-color:pink;width:100%">
+    <th style="width:100%"><center><h1 style="border: 5px solid black;">_MOBILES_</h1></center></th> 
+  </tr>
+ </table><br>
+<div style="width:100%; display:flex">
+ <img  src="mobi.jpg" alt="Trulli" width="50%" height="550">
+ <img src="mob.jpg"alt="Trulli" width="50%" height="550">
+</div>
+<div  style="border: 10px solid black;" > <TR><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d464.95124752140447!2d81.39221853062364!3d21.207647132006482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a29239226ea2135%3A0xa5055005f3fdb1f1!2sMOBILE%20JUNCTION!5e0!3m2!1sen!2sin!4v1698400637502!5m2!1sen!2sin" width="100%" height="450" style="border:4px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></TR></div>
+<table style="width: 100%" class="table table-hover">
+  <tr style="background-color:pink;width:100%">
+    <th style="width:100%"><center><h1  style="border: 5px solid black;">_ALL_TYEPS_TABLET_SALCE_AND_REPAYREING_</h1></center></th> 
+  </tr>
+</table><br>
+<div style="width:100%; display:flex">
+ <img src="tablet.jpg"alt="Trulli" width="100%" height="100%">
+</div>
+<table style="width: 100%" class="table table-hover">
+  <tr style="background-color:pink;width:100%">
+    <th style="width:100%"><center><h1 style="border: 5px solid black;">_LAPTOPS_</h1></center></th> 
+</tr>
+   <tr>
+    <th>
+    	<h1>Laptopparts.in is a Nehru Place based Laptop & Desktop spare parts manufacture, wholesaler, retailer,  online seller distributor  as well, Laptop and Desktop vendor can be our distributer, import, export via Laptex Impex (Laptopparts.in), For the retailer user they can buy multiple or Single product from our ecommerce website portal, wholesaler, shopkeeper retailer can Place bulk order @minimum and chip price. Our Laptop, Desktop spare parts & components are 100% Genuine and compatible, we are selling all type of laptop parts and there components.
+    	</h1>
+    </th>
+</tr>
+
+<tr>
+  <th>
+	<h1>Laptop & Desktop spare parts, Battery, Laptop Adapter, Laptop Dc Jack, Laptop & Desktop Hdd Connector, Laptop Screen Panel, Laptop Base Cover, Laptop & Desktop Display Cable, Laptop Hinges, V Keyboard, Laptop Heatsink, Laptop Lcd Top Cover, Laptop Screen Bezel, Laptop Touchpad Cover, Laptop & Desktop Power On Off, Laptop Daughter Card, Laptop Internal Camera, Ram, Motherboard, Laptop Fan, Laptop Speakers.
+	</h1>
+  </th>
+ </tr>
+</table><br>
+<div style="width:100%;display:flex ">
+	<img src="laptop1.jpg"alt="Trulli" width="50%" height="550">
+ <img  src="laptop.jpg" alt="Trulli" width="50%" height="550">
+ </div>
+ <table style="width: 100%" class="table table-hover">
+  <tr style="background-color:pink;width:100%">
+    <th style="width:100%"><center><h1 style="border: 5px solid black;">_computer_&_PC_</h1></center></th> 
+  </tr>
+  <tr>
+  <th>
+	<h1>Laptop & Desktop spare parts, Battery, Laptop Adapter, Laptop Dc Jack, Laptop & Desktop Hdd Connector, Laptop Screen Panel, Laptop Base Cover, Laptop & Desktop Display Cable, Laptop Hinges, V Keyboard, Laptop Heatsink, Laptop Lcd Top Cover, Laptop Screen Bezel, Laptop Touchpad Cover, Laptop & Desktop Power On Off, Laptop Daughter Card, Laptop Internal Camera, Ram, Motherboard, Laptop Fan, Laptop Speakers.
+	</h1>
+  </th>
+ </tr>
+</table><br>
+<div style="width:100%; display:flex">
+ <img  src="pc.jpg" alt="Trulli" width="50%" height="550">
+ <img src="pc1.jpg"alt="Trulli" width="50%" height="550">
+
+</div>
+<div><img src="pc4.jpg"alt="Trulli" width="100%" height="600"></div>
+<table style="width: 100%" class="table table-hover">
+  <tr style="background-color:pink;width:100%">
+    <th style="width:100%"><center><h1 style="border: 5px solid black;">News paper</h1></center></th> 
+  </tr>
+  <th style="width:100%"  style="border: 3px solid black;"><center><h1>Ravi News Agency</h1></center></th> 
+  </tr>
+ </table><br>
+
+<div style="width:100%; display:flex" "border: 3px solid black;">
+ <img  src="p1.jpg" alt="Trulli" width="50%" height="550">
+ <img src="p2.jpg"alt="Trulli" width="50%" height="550">
+  </div>
+  <div> 
+  	<table> 
+  		<tr><img src="p4.jpg"alt="Trulli" width="50%" height="550" style="float: right;height: 617px;width: 518px;margin-top:-41px">
+  		</tr>
+  		<tr>
+ <h2>हिन्दी <br>
+फरीदाबाद की आवाज़<br>
+नवभारत टाइम्स<br>
+दैनिक जागरण<br>
+हिन्दुस्तान<br>
+दैनिक भास्कर<br>
+अमर उजाला<br>
+मिशन जयहिन्द<br>
+राष्ट्रीय सहारा</h2><br></tr>
+<tr><tr><tr><img src="p5.jpg"alt="Trulli" width="50%" height="550" style="float: right;height: 617px;width: 518px;margin-top:-41px">
+  		</tr></tr>
+<h2>Language of publication	Founded<br>
+The Hindu<br>	
+Hindustan Times	<br>
+The Indian Express	<br>
+The Morung Express	<br>
+The Times Indian</h2><br></tr>
+</table>
+</div>
+<div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d398.25721254339766!2d81.392655936911!3d21.20761403170251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a2923b3519ddacb%3A0xf797dc415b773c77!2sRavi%20News%20Agency!5e0!3m2!1sen!2sin!4v1698935035734!5m2!1sen!2sin" width="100%" height="450" style="border:4px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+<table>
+<div>
+	<th style="color: "><img src="RAVI.png" id="img4" class="leftlogo"></th>
+                  <td style="width:100%"> 
+                  	<h1>RAVI ENTERPRISE</h1>
+         <img src="login.png"alt="Trulli" width="50%" height="10%" style="float: right;height: 50px;width: 50px;margin-top:-41px"> 
+         <h3 style="float: right;margin-right: -45px;margin-top: 20px;margin-left">RAVI ENTERPRISES</h3>
+       <h3 style="float: right;margin-top: -159px margin-top:margin-right:38px;margin-top: 56px;margin-right: -126px;">Sales $ Service</h3>
+        <h3 style="float: right;margin-right: -180px margin-top:margin-right:180px;margin-top: 92px;margin-right: -184px;">Khursipar Bhilai (C.G)</h3>
+                  	<H4`>BANSI BAZAR KHURSIPAR BHILAI</H4>
+                  	<H4>CHATTISGARH</H4>
+                  	<H4>DURG,CHATTISGARH</H4>
+              <H4>MOBIL:- 8982853034   EMAIL:-MRRAVI3034@GMAIL.COM</H4>
+</div></table>
+
+ </body>
+</html>
+
